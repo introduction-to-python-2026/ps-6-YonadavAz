96a4a3398007bde42984d1437e103a4c03c2028a
Yonadav Azaria <yonadav.azaria@gmail.com>
2025-11-27 21:55:38 +0200

    create: create_codon_dict

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    path = {}
+    # file = open(file_path)
+    # rows = file.readlines()
+    # file.close()
+    with open( file_path) as file:
+        rows = file.readlines()
+    for row in rows:
+        row.strip().split("/t")
+        path = create_codon_dict(row)
+    return path
 
-
+if
